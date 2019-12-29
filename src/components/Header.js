3c54c2993cc614f9c299876dfa53706602752f69
unknown <dudusabag73@gmail.com>
2019-12-29 23:51:42 +0200

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,45 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 
-
-
-class Header extends React.Component {
-    render() {
-        return (
+const Header = () => {
+    return (
+        <>
             <div className="header">
                 <h1 className="herolo-weather-task">Herolo Weather Task</h1>
-                <ButtonToolbar>
-                    <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                        <ToggleButton value={1}>Radio 1 (pre-checked)</ToggleButton>
-                        <ToggleButton value={2}>Radio 2</ToggleButton>
+                <div className="btn-container">
+                    <ButtonToolbar >
+                        <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                            <ToggleButton value={1}>Home</ToggleButton>
+                            <ToggleButton value={2}>Favorites</ToggleButton>
 
-                    </ToggleButtonGroup>
-                </ButtonToolbar>
+                        </ToggleButtonGroup>
+                    </ButtonToolbar>
+                </div>
             </div>
-        );
-    }
+
+        </>
+    )
 }
+
+
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <div className="header">
+//                 <h1 className="herolo-weather-task">Herolo Weather Task</h1>
+//                 <div className="btn-container">
+//                     <ButtonToolbar >
+//                         <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+//                             <ToggleButton value={1}>Home</ToggleButton>
+//                             <ToggleButton value={2}>Favorites</ToggleButton>
+
+//                         </ToggleButtonGroup>
+//                     </ButtonToolbar>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 export default Header;
