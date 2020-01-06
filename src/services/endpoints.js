c08502c1e4feeed6b882d80700f91469f44e81c7
unknown <dudusabag73@gmail.com>
2020-01-06 02:30:54 +0200

    https

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,10 +1,10 @@
 const apiKey ="VdYSGQ8CMzlgGl7hQ7QADNiKwvkKwNAS"
  
 const endPointes = {
-    autocomplete: (q) => `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${q}`,
-    getCurrentWeather: (locationKey) => `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`,
-    fiveDaysDailyForecasts: (locationKey) => `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`,
-    geoPositonSearch: (lat,lon) => `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat}%2C${lon}`,
+    autocomplete: (q) => `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${q}`,
+    getCurrentWeather: (locationKey) => `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`,
+    fiveDaysDailyForecasts: (locationKey) => `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`,
+    geoPositonSearch: (lat,lon) => `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat}%2C${lon}`,
     getLatLon: new Promise((resulve) => {    
         navigator.geolocation.getCurrentPosition(coords => resulve(coords))
             
